a1c10ccff3eb80f9c9634ad016513d9a5bb1646d
Saad <a771367@gmail.com>
2020-05-05 06:38:56 +0300

    s

--- PrayerTime-SwiftUI/ContentView.swift
+++ PrayerTime-SwiftUI/ContentView.swift
@@ -10,7 +10,131 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            
+            HStack {
+                VStack{
+                    
+                      Spacer()
+                    
+                    
+                    
+                    
+                    
+                    HStack {
+                        Text("باقي على الاذان")
+                            .font(.title)
+                        .bold()
+                    }
+                    HStack {
+                        Text(" <")
+                            .font(.largeTitle)
+                        Spacer()
+                     Text("٥ مايو - ١٢ رمضان")
+                        
+                        .font(.title)
+                        .bold()
+                        Spacer()
+                        Text("> ")
+                                                   .font(.largeTitle)
+                       
+                        
+                        
+                            
+                    }.background(Color.gray)
+                        
+                       
+                    
+                    
+//                    Spacer()
+//                    HStack {
+//
+//                        background(Color.gray)
+//                    Text("٥ مايو - ١٢ رمضان")
+//                        .padding(.bottom,15)
+//                        .font(.largeTitle)
+//
+//
+//                    }
+                    
+                }
+            }
+            HStack {
+                
+                VStack {
+                   
+                    HStack {
+                        Spacer()
+                        Text("٣.٣٨ ص")
+                            .font(.largeTitle)
+                            .bold()
+                        Spacer()
+                        Text("الفجر")
+                            .font(.largeTitle)
+                            .bold()
+                        Spacer()
+                    }
+                    Spacer()
+                    HStack {
+                        Spacer()
+                        Text("٣.٣٨ ص")
+                            .font(.largeTitle)
+                            .bold()
+                        Spacer()
+                        Text("الظهر")
+                            .font(.largeTitle)
+                            .bold()
+                        Spacer()
+                    }
+                    Spacer()
+                    HStack {
+                        Spacer()
+                        Text("٣.٣٨ ص")
+                            .font(.largeTitle)
+                            .bold()
+                        Spacer()
+                        Text("العصر")
+                            .font(.largeTitle)
+                            .bold()
+                        Spacer()
+                    }
+                    Spacer()
+                    HStack {
+                        Spacer()
+                        Text("٣.٣٨ ص")
+                            .font(.largeTitle)
+                            .bold()
+                        Spacer()
+                        Text("المغرب")
+                            .font(.largeTitle)
+                            .bold()
+                        Spacer()
+                    }
+                    Spacer()
+                    HStack {
+                        Spacer()
+                        Text("٣.٣٨ ص")
+                            .font(.largeTitle)
+                            .bold()
+                        Spacer()
+                        Text("العشاء")
+                            .font(.largeTitle)
+                            .bold()
+                        Spacer()
+                    }
+                    Spacer()
+                    
+                }
+                
+                
+                
+            }
+            
+           
+        }
+        .background(Color.red)
+        .edgesIgnoringSafeArea(.all)
+            
     }
 }
 
